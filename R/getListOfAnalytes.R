ea97d1f4805b2229225049693de988d0441fa7f1
Tylar <murray.tylar@gmail.com>
2025-05-28 17:00:21 -0400

    start on analyte reports. improve index.md

---
+++ R/getListOfAnalytes.R
@@ -0,0 +1,26 @@
+getListOfAnalytes <- function(){
+    # NOTE: instead of reading all data here, we hardcode the list of analytes
+    analytes <- c(
+        "Ammonia (N)", 
+        "Nitrite (NO2)", 
+        "Nitrate (NO3)", 
+        "Nitrate + Nitrite (NO3 + NO2)", 
+        "Orthophosphate (PO4)", 
+        "Silicate (Si)", 
+        "Chlorophyll a (chl-a)", 
+        "Phytoplankton (phaeo)"
+    
+    # Turbidity
+    # Nitrogen, Ammonia
+    # Nitrogen, Kjeldahl, Total
+    # Nitrogen, NO2 plus NO3
+    # Phosphorus, Total (as P) LL
+
+    # #These are secondary (grab as possible)
+    # Salinity
+    # Oxygen, Dissolved
+    # Field pH
+    # Field Temperature
+    )
+    return(analytes)
+}
