f3cf6a147a8d76153673eeeb6da3da4897944404
Tylar <murray.tylar@gmail.com>
2025-08-21 16:29:02 -0400

    fix STORET->SEACAR confusion. Specify render order. + SEACAR-check report.

--- R/mutateWINToSEACAR.R
+++ R/mutateWINToSEACAR.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-mutateWINToSTORET <- function(df, .keep = "none"){
+mutateWINToSEACAR <- function(df, .keep = "none"){
   # map WIN column names to post-2025 reporting columns
   # specified by 
   # excel file SEACAR_Metadata_UnifiedWQ_ID10006.xlsx
@@ -18,73 +18,47 @@ mutateWINToSTORET <- function(df, .keep = "none"){
   # It is noted below how many rows may have information to contribute.
   df %>%
     mutate(
-      RowID = RowID,
-      ProgramID = ProgramID,
-      
+      # mapping from WIN column names to SEACAR names
       ProgramName = program,
-
-      Habitat = Habitat,
-
-      IndicatorID = IndicatorID,
-      
-      IndicatorName = IndicatorName,
-      
-      ParameterID = ParameterID,
-      
       ParameterName = DEP.Analyte.Name,
-      
-      ParameterUnits = DEP.Result.Unit,
-      
       ProgramLocationID = Monitoring.Location.ID,
-      
-      AreaID = AreaID,
-      
-      ManagedAreaName = ManagedAreaName,
-      
-      Region = Region,
-      
       ActivityType = Activity.Type,
-      
-      
       SampleDate = Activity.Start.Date.Time,
-      
       ResultValue = DEP.Result.Value.Number,
-      
-      Year = Year,
-      
-      Month = Month,
-      
       ActivityDepth_m = Activity.Depth,
-      
-      RelativeDepth = RelativeDepth,
-      TotalDepth_m = TotalDepth_m,
-      
-      MDL = MDL,
-      
-      PQL = PQL,
-      
-      DetectionUnit = DetectionUnit,
-      
       ValueQualifier = Value.Qualifier,
-      
-      ValueQualifierSource = ValueQualifierSource,
-      
       ResultComments = Result.Comments,
-      
       OriginalLatitude = Org.Decimal.Latitude,
       OriginalLongitude = Org.Decimal.Longitude,
       
+      # unchanged values. these are likely only filled for data being
+      # loaded from SEACAR historical data files.
+      # There is a chance of collision here, which would only be a problem
+      # if the vocabulary of the two data sources was different for the two
+      # columns with the same name.
+      RowID = RowID,
+      ProgramID = ProgramID,
+      SEACAR_EventID = SEACAR_EventID,
+      Habitat = Habitat,
+      IndicatorID = IndicatorID,
+      IndicatorName = IndicatorName,
+      ParameterID = ParameterID,
+      ParameterUnits = DEP.Result.Unit,
+      AreaID = AreaID,
+      ManagedAreaName = ManagedAreaName,
+      Region = Region,
+      Year = Year,
+      Month = Month,
+      RelativeDepth = RelativeDepth,
+      TotalDepth_m = TotalDepth_m,
+      MDL = MDL,
+      PQL = PQL,
+      DetectionUnit = DetectionUnit,
+      ValueQualifierSource = ValueQualifierSource,
       SEACAR_QAQCFlagCode = SEACAR_QAQCFlagCode,
-      
       SEACAR_QAQC_Description = SEACAR_QAQC_Description,
-      
       Include = Include,
-      
-      SEACAR_EventID = Activity.ID,
-      
       MADup = MADup,
-      
       ExportVersion = ExportVersion,
-      
       .keep = .keep)
 }
